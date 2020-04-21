732eb805dce21e9050ddab2e3b76f80b8abc7719
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-04-21 09:17:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,4 +13,15 @@ if (isset($_GET["file"])) {
   fwrite($myfile, $file);
   fclose($myfile);
 }
+if (isset($_GET["download"])) {
+  $source = $_GET['download'];
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $source);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_SSLVERSION,3);
+  $data = curl_exec ($ch);
+  $error = curl_error($ch); 
+  curl_close ($ch);
+  echo $data;
+}
 ?>
